ca6a555fac294a59a1aa74a55801392f4b392029
Erik Maide <maide.erik@gmail.com>
2020-03-06 13:14:07 +0200

    braking code

--- sum.js
+++ sum.js
@@ -1,6 +1,6 @@
 function randonthindgmidagi(a = 0, b = 'kala') {
   const a1 = Number(a);
-  const b1 = Number(b);
+  const b1 = Number(b)...
   if (Number.isNaN(a1) || Number.isNaN(b1)) {
     throw new Error('Invalid arguments');
   }
